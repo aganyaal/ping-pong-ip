ab0a4c37e5dfdfb64c009bb265010504adcc61ce
Al Aganya <tbone.aa87@gmail.com>
2017-02-24 16:29:24 +0300

    Completion

---
+++ README.md
@@ -0,0 +1,10 @@
+#PING PONG SITE
+This site is a simple mechanism that uses Jquery to make magic of numbers.
+It uses some basic guidelines:
+1. Numbers divisible by three are replaced by "ping"
+2. Numbers divisible by 5 are replaced by "pong"
+3. Numbers divisible by 15 are replaced by "ping pong"
+
+##AUTHOR:Al Aganya
+
+__subject to correction__
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
-$(document).ready(function(){
-  var num= $('input#input1').val();
-  $("form#form1").submit(function(event){
-    event.preventDefault();
-    if (num=3) {
-      $("#ul").append("<li>"+ num +"</li>")
+// back-end logic
+var pingit = function(number) {
+    var product = [];
+    for (var i = 1; i <= number; i++) {
+        if ((i % 3 == 0) && (i % 5 == 0)) {
+            product.push("Ping Pong");
+        } else if (i % 3 == 0) {
+            product.push("ping");
+        } else if (i % 5 == 0) {
+            product.push("pong");
+        } else {
+            product.push(i);
+        }
     }
+    return product;
+};
 
-  });
+// Front-end Logic
+$(document).ready(function() {
+    $("div.bodii").hide();
+    $("form#form1").submit(function() {
+        event.preventDefault();
+        var number = parseInt($("input#input1").val());
+        var output = pingit(number);
+        output.forEach(function(num) {
+            $("ul#ul").append("<li>" + num + "</li>")
+        });
+        $("div.hiding").hide();
+        $("div.bodii").fadeIn(1000).fadeOut(1000);
+        $("div.hiding").fadeIn(4000);
+    });
 });
